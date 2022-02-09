1872fd7b6598af6630b47f8b7d185efb879841f8
Travis Amadasun <amadasut@bswiesaude.onmicrosoft.com>
2022-02-09 15:08:43 +0100

    Projektdateien hinzufügen.

---
+++ Roulette/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie Ihren wetteinsatz an");
+            Console.WriteLine("Geben Sie ihre Zahl an");
+            Console.WriteLine("Geben Sieihre Farbe an");
+        }
+    }
+}
---
+++ Roulette/Spieler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    public delegate void InvaildMesureEventHandler();
+    public class Spieler : EventArgs
+    {
+        public event InvaildMesureEventHandler InvaildMeasure;
+        private string _name;
+        private int _zahl;
+        private string _farbe;
+        private int _einsatz;
+        private double _guthaben;
+        public double Guthaben { get; set; }
+        //Guthaben wird draufaddiet abgezoigen
+        //spiellogik loop runde roundstart event einsatz wier agefragt  
+        //event für spiel 
+        //postfreundevent 
+        //Abprüfungsevent
+        //eventhandling
+        //rotschwarz
+        //gerade undgerade
+        public string Name { 
+            get => _name;
+            set
+            {
+                if(_name != null)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    InvaildMeasure();
+                }
+            }
+        }
+
+        public int Zahl { get; set; }
+
+        public string Farbe { get; set; }
+
+        public int Einsatz { get; set; }
+    }
+}
